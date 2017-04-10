f2f9b81076cabefa48b78d19f2565e70b4b2c539
mpfleger <michaela.pfleger@elements.at>
2017-04-10 22:30:58 +0200

    Layout - Index Page - Personal Info added

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,9 @@ var config = require('./webpack.config.js');
 var named = require('vinyl-named');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var jimp = require('gulp-jimp');
+
 
 gulp.task('css', function () {
     gulp.src('src/css/**/*.css')
@@ -19,6 +22,18 @@ gulp.task('css', function () {
         }))
         .pipe(cssmin())
         .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('img', function () {
+    gulp.src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('profile-img', function () {
+    gulp.src('src/img/profile/**/*')
+        // .pipe(jimp({ greyscale: true }))
+        .pipe(gulp.dest('build/img/profile'));
 });
 
 gulp.task('scss', function () {
@@ -62,11 +77,12 @@ gulp.task('watch',function () {
     gulp.watch('src/js/**/*',['js']);
     gulp.watch('src/scss/**/*',['scss']);
     gulp.watch('src/css/**/*',['css']);
+    gulp.watch('src/img/**/*',['img']);
     gulp.watch('src/templates/**/*',['templates']);
 });
 
 
-gulp.task('default', ['css','scss','js', 'templates']);
+gulp.task('default', ['css','scss','js', 'img', 'profile-img', 'templates']);
 gulp.task('dev', ['default', 'serve', 'watch']);
 
 
