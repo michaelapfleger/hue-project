d402996bb61ce9cdf1b3837d11b688d7bf5af87a
mpfleger <michaela.pfleger@elements.at>
2017-04-24 02:36:12 +0200

    Firebase Config (normally I would not put that into git but otherwise you have no database access ;) )

---
+++ src/js/firebase.config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyANdPf0Q_JQPXLKsNDI6Bvqy5nN5il59gI",
+    authDomain: "diary-hue.firebaseapp.com",
+    databaseURL: "https://diary-hue.firebaseio.com",
+    projectId: "diary-hue",
+    storageBucket: "gs://diary-hue.appspot.com",
+    messagingSenderId: "741549784015"
+};
+
+export default config;
